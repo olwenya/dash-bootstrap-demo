56765c9dc3ad68a2329986bafa9567081ed0a18e
Allan Olweny <allan.olweny@gmail.com>
2021-04-18 13:38:59 -0400

    Add Jumbotron

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import dash
 import dash_bootstrap_components as dbc
+import dash_html_components as html
 
 app = dash.Dash(external_stylesheets=[dbc.themes.CYBORG])
 
 app.layout = dbc.Container(
-    dbc.Alert("Hello Neighbor", color="success")
+    dbc.Jumbotron(children=[
+        html.H1("HELLO NEIGHBOR"),
+        html.P('dash bootstrap demo')
+    ])
 )
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
